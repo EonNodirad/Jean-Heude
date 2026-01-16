5eef1dbc878583b2af6e3c549849944ddc9de459
Noé <noe.daridon@gmail.com>
2026-01-16 19:47:43 +0100

    le début

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	let messages = [{ role: 'assistant', content: 'Salut ! je suis ton goat JEAN-Heude' }];
+	let currentMessage = '';
+</script>
+
+<h1>salut c'est JEAN-Heude</h1>
+
+<div class="chat-widows">
+	{#each messages as msg}
+		<p class={msg.role}>
+			<strong>{msg.role === 'user' ? 'Toi' : 'IA'}</strong>
+			{msg.content}
+		</p>
+	{/each}
+</div>
+
+<style>
+	.chat-widows {
+	}
+</style>
