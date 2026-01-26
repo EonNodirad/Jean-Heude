18b81461687a32061d05b621a42a9f09b97fa3a5
Noé <noe.daridon@gmail.com>
2026-01-26 16:45:46 +0100

    docker good very good

--- frontend/.svelte-kit/adapter-node/chunks/public.js
+++
@@ -1,4 +0,0 @@
-const PUBLIC_URL_SERVEUR_PYTHON = "http://localhost:8000";
-export {
-  PUBLIC_URL_SERVEUR_PYTHON as P
-};
--- frontend/build/server/chunks/1-g1libGto.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-CQK1mAmE.js')).default;
-const imports = ["_app/immutable/nodes/1.efNGcszO.js","_app/immutable/chunks/8QMswpyS.js","_app/immutable/chunks/YeR_7YQO.js","_app/immutable/chunks/B7mEjzpW.js","_app/immutable/chunks/Da7Di7hP.js","_app/immutable/chunks/6vUFCIRl.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-g1libGto.js.map
